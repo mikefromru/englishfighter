aa067b5a819ffb3238e8bdfe17a23dbae6ecb8a7
googlockgood@gmail.com <googlockgood@gmail.com>
2017-04-10 19:54:55 +0300

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^profile/$', profile),
+    url(r'^editing/$', editing_profile),
 ]
--- fighter/models.py
+++ fighter/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     city = models.CharField(max_length=30)
+    about = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', verbose_name='Изображения', blank=True, null=True)
 
     def __unicode__(self):
-        return self.avatar
+        return self.user
 
     class Meta:
         verbose_name = 'Профиль'
